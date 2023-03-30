cdd15c61bb5b2a0e98166403153304648c33ff0c
OluwaseunSoft <Oluwaseunazure@yahoo.com>
2023-03-30 21:38:01 +0100

    test method

---
+++ test/ToDoAPI.Tests/ToDoModelTests.cs
@@ -0,0 +1,22 @@
+using ToDoAPI.Models;
+
+namespace ToDoAPI.Tests;
+
+public class ToDoModelTests
+{
+    [Fact]
+    public void CanChangeName()
+    {
+        //Arrange
+        var testToDo = new ToDoItem
+        {
+            Name = "Complete Unit Tests"
+        };
+
+        //Act
+        testToDo.Name = "Error error";
+
+        //Assert
+        Assert.Equal("Error error", testToDo.Name);
+    }
+}
